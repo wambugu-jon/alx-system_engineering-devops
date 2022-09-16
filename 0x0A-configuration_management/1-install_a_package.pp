e225b5350a92fe735948fd51e0481a3b3cda377d
wambugu-jon <wambugu-jon@github.com>
2022-09-16 23:56:29 +0300

    CONFIG MGMT.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# installs the package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# installs flask -v 2.1.0
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
